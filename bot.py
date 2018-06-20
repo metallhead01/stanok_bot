073664eb162e4bb9187453813f13d1dc715ab566
None <non@non.com>
2018-06-20 22:19:32 +0300

    Commit.

--- bot.py
+++ bot.py
@@ -1,63 +1,74 @@
+import email_handler
+import time
+import requests
 import telebot
 from telebot.types import Message
-from telebot import types
 import keyboard_buttons
+
 
 
 with open('telegram.token', 'r') as f:
     token = f.read()
-TOKEN = token
-STICKER_ID = 'CAADAgADEQIAAvJ-ggwzgX6aU1rEAwI'
-bot = telebot.TeleBot(TOKEN)
 
+TOKEN = token
+MAIN_URL = f'https://api.telegram.org/bot{TOKEN}'
+
+bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def command_handler(message: Message):
     keyboard = keyboard_buttons.level_1_menu()
-    bot.send_message(message.chat.id, "Выберите вариант:", reply_markup=keyboard)
+    bot.send_message(message.chat.id, 'Выберите вариант:', reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda x: True)
 def callback_handler(callback_query):
     message = callback_query.message
     data = callback_query.data
-    if data == 'Problem with machine':
-        keyboard = keyboard_buttons.level_2_menu()
-        bot.send_message(message.chat.id, "Укажите причину:", reply_markup=keyboard)
-
-"""
-# Объявим функцию, которая будет отвечать на сообщения
-@bot.edited_channel_post_handler(content_types=['text']) # одну и ту же функцию мы можем декорировать несколькими декораторами
-@bot.message_handler(content_types=['text'])
-def echo_messages(message: Message):
-    print(message.text)
-    text = message.text
-    if text == '1. Станки':
-        bot.send_message(message.chat.id, "Выберите модель:", reply_markup=level_2_stanki)
-        bot.register_next_step_handler(message.chat.id, "Выберите модель:", reply_markup=level_2_stanki)
-    elif text == 'Highflex 1650 FC':
-        bot.send_message(message.chat.id, "Выберите причину:", reply_markup=level_3_stanki)
-    elif text == 'HPL 300/38/22':
-        bot.send_message(message.chat.id, "Выберите причину:", reply_markup=level_3_stanki)
-    elif text == 'KTD 720':
-        bot.send_message(message.chat.id, "Выберите причину:", reply_markup=level_3_stanki)
-    elif text == 'Назад':
-        bot.send_message(message.chat.id, "Выберите вариант:", reply_markup=level_1_menu)
+    if data == 'problem_with_machine':
+        keyboard = keyboard_buttons.level_2_machine_errors()
+        bot.send_message(message.chat.id, 'Укажите причину:', reply_markup=keyboard)
+    elif data == 'wrong_task':
+        keyboard = keyboard_buttons.level_2_task()
+        bot.send_message(message.chat.id, 'Укажите причину:', reply_markup=keyboard)
+    elif data == 'warehouse':
+        keyboard = keyboard_buttons.level_2_warehouse()
+        bot.send_message(message.chat.id, 'Укажите причину:', reply_markup=keyboard)
+    elif data == 'operators':
+        keyboard = keyboard_buttons.level_2_operators()
+        bot.send_message(message.chat.id, 'Укажите причину:', reply_markup=keyboard)
+    elif data == 'no_task':
+        pass
+    elif data == 'back':
+        keyboard = keyboard_buttons.level_1_menu()
+        bot.send_message(message.chat.id, 'Выберите вариант:', reply_markup=keyboard)
     else:
-        bot.send_message(message.chat.id, "Неверный выбор")
+        pass
+
+
+while True:
+    li_st = email_handler.read_email_from_gmail()
+    i = 0
+    if li_st:
+        while i <= len(li_st):
+            element = li_st.pop()
+            payload = {
+                'chat_id': '149017157',
+                'text': element[2],
+            }
+            try:
+                r = requests.post(f'{MAIN_URL}/sendMessage', data=payload)
+                i += 1
+            except Exception as e:
+                print(str(e))
+
+        bot.polling(timeout=60)  # Настройки таймаута, выбираем в зависимости от качества Интернет-соединения timeout=60
+    else:
+        print('wait')
+
+    time.sleep(60)
 
 
 
-# Попробуем отправить стикер
-@bot.message_handler(content_types=['sticker'])
-def sticker_handler(message: Message):
-    print(message)
-    print(message.sticker)
-    print(message.chat.id)
-    # bot.send_sticker(message.chat.id, STICKER_ID) # если нужно отвечать конкретным стикером
-    bot.send_sticker(message.chat.id, message.sticker)
-
-"""
-bot.polling(timeout=60)  # Настройки таймаута, выбираем в зависимости от качества Интернет-соединения timeout=60
 
--- keyboard_buttons.py
+++ keyboard_buttons.py
@@ -47,11 +47,11 @@ operator_3 = types.KeyboardButton('Назад')
 def level_1_menu():
     level_1_menu_keyboard = types.InlineKeyboardMarkup()
     level_1_menu_buttons = [
-        types.InlineKeyboardButton(text='1. Проблема со станком', callback_data='Problem with machine'),
-        types.InlineKeyboardButton(text='2. Нечеткое задание (проблемы оформления)', callback_data='Wrong task'),
-        types.InlineKeyboardButton(text='3. Склад', callback_data='Warehouse'),
-        types.InlineKeyboardButton(text='4. Операторы', callback_data='Operators'),
-        types.InlineKeyboardButton(text='5. Нет задания – огрехи планирования', callback_data='No task')]
+        types.InlineKeyboardButton(text='1. Проблема со станком', callback_data='problem_with_machine'),
+        types.InlineKeyboardButton(text='2. Нечеткое задание (проблемы оформления)', callback_data='wrong_task'),
+        types.InlineKeyboardButton(text='3. Склад', callback_data='warehouse'),
+        types.InlineKeyboardButton(text='4. Операторы', callback_data='operators'),
+        types.InlineKeyboardButton(text='5. Нет задания – огрехи планирования', callback_data='no_task')]
     level_1_menu_keyboard.add(level_1_menu_buttons[0])
     level_1_menu_keyboard.add(level_1_menu_buttons[1])
     level_1_menu_keyboard.add(level_1_menu_buttons[2])
@@ -60,7 +60,7 @@ def level_1_menu():
     return level_1_menu_keyboard
 
 
-def level_2_menu():
+def level_2_machine_errors():
     level_2_menu_keyboard = types.InlineKeyboardMarkup()
     level_2_menu_buttons = [
     types.InlineKeyboardButton(text='a. Компьютер завис', callback_data='level_2_PC_down'),
@@ -70,7 +70,7 @@ def level_2_menu():
     types.InlineKeyboardButton(text='e. Радиусная обработка фрезами', callback_data='level_2_Radius'),
     types.InlineKeyboardButton(text='f. Раунд – фрезы', callback_data='level_2_Round'),
     types.InlineKeyboardButton(text='g. Цикли', callback_data='level_2_Cicles'),
-    types.InlineKeyboardButton(text='Назад', callback_data='level_2_Back')]
+    types.InlineKeyboardButton(text='Назад', callback_data='back')]
     level_2_menu_keyboard.add(level_2_menu_buttons[0])
     level_2_menu_keyboard.add(level_2_menu_buttons[1])
     level_2_menu_keyboard.add(level_2_menu_buttons[2])
@@ -82,46 +82,41 @@ def level_2_menu():
     return level_2_menu_keyboard
 
 
-"""
-menu_1 = types.InlineKeyboardButton(text='1. Проблема со станком', callback_data='1. Проблема со станком')
-menu_2 = types.InlineKeyboardButton(text='2. Нечеткое задание (проблемы оформления)', callback_data='2. Нечеткое задание (проблемы оформления)')
-menu_3 = types.InlineKeyboardButton(text='3. Склад', callback_data='3. Склад')
-menu_4 = types.InlineKeyboardButton(text='4. Операторы', callback_data='4. Операторы')
-menu_5 = types.InlineKeyboardButton(text='5. Нет задания – огрехи планирования', callback_data='5. Нет задания – огрехи планирования')
+def level_2_task():
+    level_2_task_keyboard = types.InlineKeyboardMarkup()
+    level_2_task_buttons = [
+        types.InlineKeyboardButton(text='a. Указать номер заказа', callback_data='set_order_number'),
+        types.InlineKeyboardButton(text='Назад', callback_data='level_2_Back_task')]
+    level_2_task_keyboard.add(*level_2_task_buttons)
+    # level_2_task_keyboard.add(level_2_task_buttons[0])
+    # level_2_task_keyboard.add(level_2_task_buttons[1])
+    return level_2_task_keyboard
 
-level_1_menu.row(menu_1, menu_2)
-level_1_menu.row(menu_3, menu_4, menu_5)
+def level_2_warehouse():
+    level_2_warehouse_keyboard = types.InlineKeyboardMarkup()
+    level_2_warehouse_buttons = [
+        types.InlineKeyboardButton(text='a. Мало кромки', callback_data='malo_kromki'),
+        types.InlineKeyboardButton(text='b. Нет кромки вообще к заказу', callback_data='net_kromki'),
+        types.InlineKeyboardButton(text='Назад', callback_data='back')]
+    level_2_warehouse_keyboard.add(level_2_warehouse_buttons[0])
+    level_2_warehouse_keyboard.add(level_2_warehouse_buttons[1])
+    level_2_warehouse_keyboard.add(level_2_warehouse_buttons[2])
+    return level_2_warehouse_keyboard
 
-level_2_stanki = types.InlineKeyboardMarkup(row_width=2)
-stanok_1_1 = types.InlineKeyboardButton('Highflex 1650 FC')
-stanok_2_1 = types.InlineKeyboardButton('HPL 300/38/22')
-stanok_3_1 = types.InlineKeyboardButton('KTD 720')
-stanok_4_1 = types.InlineKeyboardButton('Назад')
-level_2_stanki.row(stanok_1_1, stanok_2_1, stanok_3_1, stanok_4_1)
 
-level_2_stanki = types.InlineKeyboardMarkup(row_width=8)
-stanok_1_2 = types.InlineKeyboardButton('a. Компьютер завис')
-stanok_2_2 = types.InlineKeyboardButton('b. Клеенаносящий узел')
-stanok_3_2 = types.InlineKeyboardButton('c. Подфуговка')
-stanok_4_2 = types.InlineKeyboardButton('d. Торцовка')
-stanok_5_2 = types.InlineKeyboardButton('e. Радиусная обработка фрезами')
-stanok_6_2 = types.InlineKeyboardButton('f. Раунд – фрезы')
-stanok_7_2 = types.InlineKeyboardButton('g. Цикли')
-stanok_8_2 = types.InlineKeyboardButton('Назад')
-level_3_stanki.add(stanok_1_2, stanok_2_2, stanok_3_2, stanok_4_2)
-level_3_stanki.add(stanok_5_2, stanok_6_2, stanok_7_2, stanok_8_2)
+def level_2_operators():
+    level_2_operators_keyboard = types.InlineKeyboardMarkup()
+    level_2_operators_buttons = [
+        types.InlineKeyboardButton(text='a. Перекур не по графику', callback_data='perekur'),
+        types.InlineKeyboardButton(text='b. Кривые руки', callback_data='krivie_ruki'),
+        types.InlineKeyboardButton(text='Назад', callback_data='back')]
+    level_2_operators_keyboard.add(level_2_operators_buttons[0])
+    level_2_operators_keyboard.add(level_2_operators_buttons[1])
+    level_2_operators_keyboard.add(level_2_operators_buttons[2])
+    return level_2_operators_keyboard
 
-level_2_zadanie = types.ReplyKeyboardMarkup()
-task_1 = types.KeyboardButton('a. Указать номер заказа')
-task_2 = types.KeyboardButton('Назад')
-
-level_2_sklad = types.ReplyKeyboardMarkup()
-storage_1 = types.KeyboardButton('a. Мало кромки')
-storage_2 = types.KeyboardButton('b. Нет кромки вообще к заказу')
-storage_3 = types.KeyboardButton('Назад')
-
-level_2_operatori = types.ReplyKeyboardMarkup()
-operator_1 = types.KeyboardButton('a. Перекур не по графику')
-operator_2 = types.KeyboardButton('b. Кривые руки')
-operator_3 = types.KeyboardButton('Назад')
-"""
+def confirm():
+    confirm_keyboard = types.InlineKeyboardMarkup()
+    confirm_buttons = [
+        types.InlineKeyboardButton(text='Да', callback_data='yes'),
+        types.InlineKeyboardButton(text='Нет', callback_data='no')]
